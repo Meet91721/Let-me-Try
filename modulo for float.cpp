d45b3c458485633024832119cb8acd30052f699c
Meet91721 <74113151+Meet91721@users.noreply.github.com>
2021-03-21 15:14:59 +0530

    Update modulo for float.cpp

--- modulo for float.cpp
+++ modulo for float.cpp
@@ -2,33 +2,25 @@
 
 using namespace std;
 
-float func(float, float);
-
+void func(float, float);
 
 int main()
 {
     cout << "A PROGRAM TO USE % FOR FLOAT IN CPP   XD\n\n\n\n";
     float a, b;
-    cout <<"Enter the 1st number:\n";
+    cout << "Enter the 1st number:\n";
     cin >> a;
     cout << "Enter the 2nd number:\n";
     cin >> b;
-
-    cout << func(a, b);
-
+    cout << "The modulo of the function is: ", func(a, b);
+    ;
 }
 
-
-float func(float a, float b)
+void func(float a, float b)
 {
     int a1 = a;
     int b1 = b;
-    if (a1 >= 0 && b1 >= 0)
-    {    
-        float ans = (a1 % b1) - (a - a1) + (b-b1)*(a1/b1);
-        if (ans >= 0)
-        return ans;
-        else
-        return (-1) * ans;
-    }
+    int divi = a / b;
+    cout << a - b * divi;
 }
+
